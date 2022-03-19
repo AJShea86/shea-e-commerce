a4095c23f32005a9803552a5143a414aebf15878
Andrew S <andrewsheamls@gmail.com>
2022-03-19 17:23:53 -0600

    tags and tag id search is working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,43 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll({
+
+      include:[{
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"]
+      }]
+    })
+    res.status(200).json(allTags)
+  } catch (error) {
+    console.log(error);
+
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
+try {
+  const singleTag = await Tag.findOne({
+    where: {id: req.params.id},
+    include: {
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"]      
+    }
+
+  })
+  res.status(200).json(singleTag)
+} catch (error) {
+  
+}
+
+
 });
 
 router.post('/', (req, res) => {
